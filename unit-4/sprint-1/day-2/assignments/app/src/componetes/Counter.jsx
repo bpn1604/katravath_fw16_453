c2febf85aaf9179cbfb761e13a962dac05d48964
prabhuRV <99705782+prabhuRV@users.noreply.github.com>
2022-04-09 22:13:18 +0530

    22.13

---
+++ unit-4/sprint-1/day-2/assignments/app/src/componetes/Counter.jsx
@@ -0,0 +1,31 @@
+
+import { useState} from 'react';
+
+function Counter() {
+    const [count, setCount]=useState(0);
+
+    const handleInc=()=>
+    {
+        setCount(count+1)
+    }
+    const handleDec=()=>
+    {
+        setCount(count-1)
+    }
+    const handleDouble=()=>
+    {
+        setCount(count*2)
+    }
+  return (
+   <>
+   <h1>Counter</h1>
+   <h1 style={{color :count%2==0 ? "green":"red"}}>{count}</h1>
+   <button onClick={()=>handleInc()}>Increment</button>
+   <button onClick={()=>handleDec()}>Decrement</button>
+   <button onClick={()=>handleDouble()}>Double</button>
+   </>
+   
+  )
+}
+
+export default Counter
