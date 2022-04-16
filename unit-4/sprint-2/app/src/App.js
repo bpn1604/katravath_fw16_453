aff7f24dc1071602e461748c73947452306bb985
prabhuRV <99705782+prabhuRV@users.noreply.github.com>
2022-04-16 23:27:41 +0530

    timer is updated

--- unit-4/sprint-2/app/src/App.js
+++ unit-4/sprint-2/app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "./Styles/Styles.css";
+import './timer.css';
 
 import { useState } from "react";
 import TodoList from "./day-1/TodoList";
 import Todo from "./day-1/Todo";
 import Form from "./day-2/Form";
+import Timer from "./day-3/Timer";
 
 function App() {
   const [showCounter, setShowCounter] = useState(true);
@@ -12,8 +14,10 @@ function App() {
     <div className="App">
       {/* <button onClick={setShowCounter(!setShowCounter)}></button> */}
       {/* <Counter/> */}
-      <Todo/>
+      {/* <Todo/> */}
       {/* <Form/> */}
+
+      <Timer />
     </div>
   );
 }
---
+++ unit-4/sprint-2/app/src/day-3/Timer.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React ,{useState,useEffect} from "react";
+
+
+const Timer = () =>{
+    const [seconds,setSeconds]=useState(0);
+    const [minutes,setMinutes]=useState(0);
+    var timer;
+    useEffect (()=>{
+        timer = setInterval(()=>{
+            setSeconds(seconds+1);
+            if(seconds===59)
+            {
+                setMinutes(minutes+1);
+                setSeconds(0);
+            }
+        },10)
+        return () =>clearInterval(timer);
+    });
+    const restart = () =>{
+        setSeconds(0);
+        setMinutes(0);
+    }
+    const stop =() =>{
+        clearInterval(timer);
+    }
+    return(
+        <div className="timer">
+            <div className="container">
+                <div className="timer_container">
+                    <h1>Timer</h1>
+                    <h1>{minutes<10?"0"+minutes:minutes}</h1>
+                    <button className="restart" onClick={restart}>restart</button>
+                    <button className="stop" onClick={stop}>stop</button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Timer
